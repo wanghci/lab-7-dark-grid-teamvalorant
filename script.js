ef4246ea2a53b987fd78aa05bae2238d08fef2c8
Asa Gittman <gittmaae@dukes.jmu.edu>
2024-02-29 15:21:43 -0500

    JS stuff

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ let dark = document.querySelector(a);
 let light = document.querySelector(a);
 
 os.addEventListener("click", function (){
+    let body = document.body;
+    body.className = "";
+})
 
+dark.addEventListener("click", function (){
+    let body = document.body;
+    body.className = "dark";
+})
+
+light.addEventListener("click", function (){
+    let body = document.body;
+    body.className = "debug";
 })
